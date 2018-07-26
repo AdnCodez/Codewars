ec6cb37c079be3d6f5ac77c7fe739c7a10611fdd
Adnan Habbat <adnan.7.habbat@gmail.com>
2018-07-26 22:58:35 +0100

    Details / Solution

---
+++ Base64Encoding.py
@@ -0,0 +1,17 @@
+# 5 kyu / Base64 Encoding
+# Details:
+#   Create a function (Python, C#) that converts the value of the String to and from Base64 using the ASCII
+# character set.
+
+from base64 import b64encode, b64decode
+
+
+def to_base_64(string):
+    return b64encode(string).rstrip('=')
+
+
+def from_base_64(string):
+    missing_padding = 4 - len(string) % 4
+    if missing_padding:
+        string += b'=' * missing_padding
+    return b64decode(string)
