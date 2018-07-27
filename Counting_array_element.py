e7aacbb15d3e1d712553e8a6a51c4bcb51bfd5e3
Adnan Habbat <adnan.7.habbat@gmail.com>
2018-07-27 02:36:49 +0100

    Details / Solution

--- Counting_array_element.py
+++ Counting_array_element.py
@@ -3,6 +3,7 @@
 #   Write a function that takes an array and counts the number of each unique element present.
 # https://www.codewars.com/kata/counting-array-elements/train/python
 
+
 def count(array):
     dic = {}
     for i in array:
