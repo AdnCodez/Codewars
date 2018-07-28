5a12b27d91498b3b11b17d1e2e7f0379ada92f93
Adnan Habbat <adnan.7.habbat@gmail.com>
2018-07-28 23:24:55 +0100

    Details / Solution

---
+++ betaex1.py
@@ -0,0 +1,15 @@
+# Beta / Testing 1-2-3
+# Details
+# https://www.codewars.com/kata/testing-1-2-3
+
+
+def number(lines):
+    ls = []
+    for i_index, i in enumerate(lines):
+        var = '{}: {}'.format(i_index + 1, i)
+        ls.append(var)
+
+    return ls
+
+
+print(number(["a", "b", "c"]))
