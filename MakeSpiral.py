dbb5203f94741313a8d9309e13cc31a31e3fc6da
Adnan Habbat <adnan.7.habbat@gmail.com>
2018-07-29 00:29:39 +0100

    Details / Solution

---
+++ MakeSpiral.py
@@ -0,0 +1,28 @@
+# 3 kyu / Make a spiral
+# Details
+#   https://www.codewars.com/kata/make-a-spiral
+
+
+def spiralize(n):
+    if n == 0:
+        return []
+
+    gridbox = [[0] * n for _ in range(n)]
+    gridbox[0] = [1] * n
+
+    s = n - 1
+    var1x, var1y = 0, n - 1
+    var2x, var2y = 1, 0
+
+    while s > 0:
+        for i in range(2 if s > 1 else 1):
+            for j in range(s):
+                var1x, var1y = var1x + var2x, var1y + var2y
+                gridbox[var1x][var1y] = 1
+            var2x, var2y = var2y, -var2x
+        s -= 2
+
+    return gridbox
+
+
+print(spiralize(5))
