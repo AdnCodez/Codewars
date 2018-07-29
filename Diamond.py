37affb45eef8aa338488ab03b17f8dbe1aaabfbe
Adnan Habbat <adnan.7.habbat@gmail.com>
2018-07-29 02:04:33 +0100

    Update Diamond.py

--- Diamond.py
+++ Diamond.py
@@ -3,6 +3,7 @@
 #   You need to return a string that displays a diamond shape on the screen using asterisk ("*") characters.
 #   https://www.codewars.com/kata/give-me-a-diamond
 
+
 def diamond(n):
     if n % 2 != 0:
         count = 1
