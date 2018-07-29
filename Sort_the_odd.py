2495c63033e765da8892e8747534c2519a8e49b9
Adnan Habbat <adnan.7.habbat@gmail.com>
2018-07-29 02:14:04 +0100

    Update Sort_the_odd.py

--- Sort_the_odd.py
+++ Sort_the_odd.py
@@ -1,11 +1,11 @@
 # 6 kyu / Sort the odd
 # Details
-# You have an array of numbers.
-# Your task is to sort ascending odd numbers but even numbers must be on their places.
-# Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
-# Example
-# sort_array([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
-# https://www.codewars.com/kata/sort-the-odd/train/python
+#  You have an array of numbers.
+#  Your task is to sort ascending odd numbers but even numbers must be on their places.
+#  Zero isn't an odd number and you don't need to move it. If you have an empty array, you need to return it.
+#  Example
+#  sort_array([5, 3, 2, 8, 1, 4]) == [1, 3, 2, 8, 5, 4]
+#  https://www.codewars.com/kata/sort-the-odd/train/python
 
 
 def sort_array(source_array):
