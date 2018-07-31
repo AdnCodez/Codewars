91c4ca501d760912b86e3c158db4de277ae79c1a
Adnan Habbat <adnan.7.habbat@gmail.com>
2018-07-31 01:27:22 +0100

    Updated Solution

--- Playig_with_digits.py
+++ Playig_with_digits.py
@@ -1,6 +1,6 @@
 # 6 kyu / Playing with digits
 # Details
-# https://www.codewars.com/kata/playing-with-digits/train/python
+#   https://www.codewars.com/kata/playing-with-digits/train/python
 
 
 def dig_pow(n, p):
